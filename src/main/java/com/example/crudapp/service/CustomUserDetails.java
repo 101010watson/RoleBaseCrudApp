da29136138ee31755bb2c90953cb87e26909baab
101010watson <abhisheksahu22999@gmail.com>
2026-08-11 00:50:59 +0530

    add CustomUserDetails for clean display of role

--- src/main/java/com/example/crudapp/service/CustomUserDetails.java
+++ src/main/java/com/example/crudapp/service/CustomUserDetails.java
@@ -1,4 +1,21 @@
 package com.example.crudapp.service;
 
-public class CustomUserDetails {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class CustomUserDetails extends User {
+    private String plainRole;
+
+    public CustomUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities, String plainRole){
+        super(username,password,authorities);
+        this.plainRole = plainRole;
+    }
+    public String getPlainRole(){
+        return plainRole;
+    }
 }
+
+// this class is created for displaying clean Role in the page
+// if not created the the sec:authentication="principal.authority" gives Role_ as the prefix which does not look good
