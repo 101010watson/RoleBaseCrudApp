f8d59eb302853eb952277185baf17208e9c5212e
101010watson <abhisheksahu22999@gmail.com>
2026-08-16 18:25:15 +0530

    update return type for save method

--- src/main/java/com/example/crudapp/service/UserService.java
+++ src/main/java/com/example/crudapp/service/UserService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface UserService {
     List<User> findAll();
     User findById(Integer id);
-    User save(User theUser);
+    void save(User theUser);
     void delete(Integer id);
     User findByEmail(String email);
     User findByPassword(String password);
